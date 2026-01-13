6f9be0c7c2585d7afa8a30a9747d7026b49b4116
Joshua Argyle <joshbudd33@gmail.com>
2026-01-12 21:02:38 -0700

    Completed the header

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,24 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+
+function parkInfoTemplate(info) {
+  return `<img class="hero" src="${info.images[0].url}" alt="Hero Banner" />
+  <div class="hero-banner__content">
+  <a class="hero_banner_title" href="#">${info.name}</a>
+  <p class="hero_banner_subtitle">
+            <span>${info.designation}</span>
+            <span>${info.states}</span>
+          </p>
+    </div>`;
+}
+
+const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+
+document.querySelector("head > title").textContent = parkData.fullName;
+
+document.querySelector(".hero-banner").innerHTML = 
+ parkInfoTemplate(parkData);
