0fc49d1e57e9a519fcf90ed4a5fe29e9efa7f6a8
Joshua Argyle <joshbudd33@gmail.com>
2026-01-30 14:07:56 -0700

    Tweaked code to try and fix it on Netify

--- src/js/main.js
+++ src/js/main.js
@@ -3,13 +3,11 @@ import { setParkIntro, setParkInfo} from "./templates.mjs"
 import { setHeaderFooter } from "./setHeaderFooter.mjs";
 
 async function init () {
-  (async () => {
   const parkData = await getParkData();
   getInfoLinks(parkData);
   setHeaderFooter(parkData);
   setParkIntro(parkData);
   setParkInfo();
-  })();
 }
 init();
   
