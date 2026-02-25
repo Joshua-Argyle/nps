6ae5f169247c019fbb582e52a20ad516d6f6284b
Joshua Argyle <joshbudd33@gmail.com>
2026-02-24 19:56:00 -0700

    Finished assignment

--- src/js/main.js
+++ src/js/main.js
@@ -10,5 +10,26 @@ async function init () {
   setParkInfo();
 }
 init();
+const button = document.querySelector("#global-nav-toggle")
+const nav = document.querySelector(".global-nav")
+const closeIcons = document.querySelector(".nav_toggle_closed")
+const openIcons = document.querySelector(".nav_toggle_opened")
+
+
+button.addEventListener('click', () => {
+  nav.classList.toggle("hidden");
+  nav.classList.toggle('nav-open');
+
+  closeIcons.classList.toggle('hidden')
+  openIcons.classList.toggle('hidden')
+
+  
+  if (nav.classList.contains('nav-open')) {
+      button.setAttribute('aria-expanded', 'false');
+  }
+  else {
+    button.setAttribute('aria-expanded', 'true');
+  }
+})
   
 
